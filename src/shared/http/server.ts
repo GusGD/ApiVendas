b4608626d5197657113518b389b5ec1332d2133b
Gustavo <si_gustavo@outlook.com>
2022-11-22 16:39:27 -0300

    erro paginação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,11 +7,13 @@ import AppError from '@shared/errors/AppError';
 import { errors } from 'celebrate';
 import '@shared/typeorm';
 import uploadConfig from '@config/upload';
+import { pagination } from 'typeorm-pagination';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
