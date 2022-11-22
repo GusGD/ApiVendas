406f2b7538387d21a14048cfbb63344eaba774bb
Gustavo <si_gustavo@outlook.com>
2022-11-22 16:22:14 -0300

    Correção no ordersRepository

--- src/modules/orders/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -16,7 +16,7 @@ interface IRequest {
 export class OrdersRepository extends Repository<Order> {
   public async findById(id: string): Promise<Order | undefined> {
     const order = this.findOne(id, {
-      relations: ['order_products', 'customers'],
+      relations: ['order_products', 'customer'],
     });
     return order;
   }
